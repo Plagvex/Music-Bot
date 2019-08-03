bbe01f085386571db27e9c127369cbe0861a1e8a
Plagvex <51000352+Plagvex@users.noreply.github.com>
2019-08-03 20:47:03 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,14 @@
+from discord.ext import commands
 
+client = commands.Bot(command_prefix=".") 
+player_dict = dict()
+
+
+@client.event
+async def on_ready():
+    print("The Bot is now Online")
+
+
+@client.command(pass_context=True)
+async def play (ctx, url):
+    channel = ctx
